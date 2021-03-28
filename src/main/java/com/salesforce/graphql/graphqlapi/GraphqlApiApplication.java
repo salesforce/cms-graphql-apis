6061f6227a8c2294491ede155da1c0a65a690ae1
Nabarun Sengupta <nsengupta@salesforce.com>
2021-03-28 09:39:03 -0700

    Exclude DataSource Au\to Configuration

--- src/main/java/com/salesforce/graphql/graphqlapi/GraphqlApiApplication.java
+++ src/main/java/com/salesforce/graphql/graphqlapi/GraphqlApiApplication.java
@@ -3,9 +3,10 @@ package com.salesforce.graphql.graphqlapi;
 import com.salesforce.graphql.SessionRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class GraphqlApiApplication {
 
     @Bean
